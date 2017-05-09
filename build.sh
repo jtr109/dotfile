a79972a17e40109fe73d7eb05536f4b215206ae7
jtr109 <lyp_login@outlook.com>
2017-05-09 23:55:58 +0800

    feat(build): build all configurations

---
+++ build.sh
@@ -0,0 +1,7 @@
+ln -s -f $(cd `dirname $0`; pwd)/oh_my_zsh/.zshrc ~/.zshrc
+ln -s -f $(cd `dirname $0`; pwd)/rime/Library/Rime/default.custom.yaml ~/Library/Rime/default.custom.yaml
+ln -s -f $(cd `dirname $0`; pwd)/rime/Library/Rime/double_pinyin_flypy.custom.yaml ~/Library/Rime/double_pinyin_flypy.custom.yaml
+ln -s -f $(cd `dirname $0`; pwd)/ss/.ShadowsocksX ~/.ShadowsocksX
+ln -s -f $(cd `dirname $0`; pwd)/vim/.vimrc ~/.vimrc
+ln -s -f $(cd `dirname $0`; pwd)/vim/.vim ~/.vim
+ln -s -f $(cd `dirname $0`; pwd)/.ackrc ~/.ackrc
