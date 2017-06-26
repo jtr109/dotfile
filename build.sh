013d1e1b9bdde89aa313bfec284aea06a787eb25
jtr109 <lyp_login@outlook.com>
2017-06-26 09:49:11 +0800

    feat(build): use nvim config in vim

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@ ln -s -f $(cd `dirname $0`; pwd)/oh-my-zsh/.zshrc $HOME
 ln -s -f $(cd `dirname $0`; pwd)/rime/Library/Rime/default.custom.yaml $HOME/Library/Rime
 ln -s -f $(cd `dirname $0`; pwd)/rime/Library/Rime/double_pinyin_flypy.custom.yaml $HOME/Library/Rime
 ln -s -f $(cd `dirname $0`; pwd)/ss/.ShadowsocksX $HOME
-ln -s -f $(cd `dirname $0`; pwd)/vim/.vimrc $HOME
-Ln -s -f $(cd `dirname $0`; pwd)/vim/.vim/.vimrc.custom $HOME/.vim
-ln -s -f $(cd `dirname $0`; pwd)/vim/.vim/.vimrc.coding $HOME/.vim
-ln -s -f $(cd `dirname $0`; pwd)/vim/.vim/.vimrc.bundles $HOME/.vim
+# ln -s -f $(cd `dirname $0`; pwd)/vim/.vimrc $HOME
+# Ln -s -f $(cd `dirname $0`; pwd)/vim/.vim/.vimrc.custom $HOME/.vim
+# ln -s -f $(cd `dirname $0`; pwd)/vim/.vim/.vimrc.coding $HOME/.vim
+# ln -s -f $(cd `dirname $0`; pwd)/vim/.vim/.vimrc.bundles $HOME/.vim
 ln -s -f $(cd `dirname $0`; pwd)/vim/.ackrc $HOME
+ln -s -f $(cd `dirname $0`; pwd)/nvim/init.vim $HOME/.vimrc  # use same config with neovim
 ln -s -f $(cd `dirname $0`; pwd)/nvim $HOME/.config
 ln -s -f $(cd `dirname $0`; pwd)/tmux/.tmux.conf.local $HOME
