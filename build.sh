a331815cb4aace1bdea56856d0e3f7001bf4032c
jtr109 <lyp_login@outlook.com>
2019-06-21 17:21:24 +0800

    all english punctuation

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 ln -s -f $(cd `dirname $0`; pwd)/oh-my-zsh/.zshrc $HOME
 ln -s -f $(cd `dirname $0`; pwd)/rime/Library/Rime/default.custom.yaml $HOME/Library/Rime
 ln -s -f $(cd `dirname $0`; pwd)/rime/Library/Rime/double_pinyin_flypy.custom.yaml $HOME/Library/Rime
+ln -s -f $(cd `dirname $0`; pwd)/rime/Library/Rime/alternative.yaml $HOME/Library/Rime
 ln -s -f $(cd `dirname $0`; pwd)/ss/.ShadowsocksX $HOME
 # ln -s -f $(cd `dirname $0`; pwd)/vim/.vimrc $HOME
 # Ln -s -f $(cd `dirname $0`; pwd)/vim/.vim/.vimrc.custom $HOME/.vim
